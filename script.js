91e81932234b804a819078b020499ccf7a4b70c3
ByteMystique <jayadevnarayanan0214@gmail.com>
2024-08-18 21:31:49 +0530

    fifth commit

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 document.getElementById('submit-comment').addEventListener('click', function() {
-    var nameInput = document.getElementById('name-input');
     var commentInput = document.getElementById('comment-input');
     var commentList = document.getElementById('comment-list');
 
-    var username = nameInput.value.trim() !== "" ? nameInput.value : "Username";
+    var username = "Username";
 
     if (commentInput.value.trim() !== "") {
         var newComment = document.createElement('li');
         newComment.innerHTML = `<strong>${username}</strong>: ${commentInput.value}`;
 
         commentList.appendChild(newComment);
-        nameInput.value = ""; // Clear the name input field
         commentInput.value = ""; // Clear the comment input field
     } else {
         alert("Please write a comment before submitting.");
